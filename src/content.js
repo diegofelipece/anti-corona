7aac214cd77b953792aa09d1548e5832d4dc2b90
Diego Esteves <diegofelipece@gmail.com>
2020-03-25 10:59:19 +0100

    feat: start using observer

--- src/content.js
+++ src/content.js
@@ -1,42 +1,78 @@
-class TextReplacer {
+const validateLang = (lang) => (
+  (lang === 'pt-BR' || lang === 'pt' || lang === 'pt-br')
+    ? 'pt-BR' : null
+)
+
+class AvoidingCorona {
   constructor() {
     this.nodes = []
-    this.listOfTextElm = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+    this.searchableNodes = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+    this.messages = {
+      'pt-BR': {
+        placeholder: 'Frescura',
+      },
+    }
   }
 
-  // TODO: get lang
+  get langMessages() {
+    const lang = document.querySelector('[lang]')
+    const language = lang && validateLang(lang.getAttribute('lang'))
 
-  // findAllNodes() {
-  //     const listOfTextElm = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
-  //     const nodes = document.querySelectorAll(listOfTextElm.join(','));
+    const msgs = this.messages[language]
+    return (language && msgs) ? msgs : null
+  }
 
-  //     this.nodes = [...this.nodes,  ...nodes];
+  appendNode(node) {
+    // check if node has corona_UID and if this id is present on nodeList
+    // if not append corona_UID to node object and set it on node list
+  }
 
-  //     // console.log('this.nodes', this.nodes);
-  //     const placeholder = 'Frescura';
+  // filterCoronaNodes(nodes) {
+  //   const { placeholder } = this.langMessages
 
-  //     for (const node of this.nodes) {
-  //         let text = node.innerText;
+  //   for (const node of nodes) {
+  //     let text = node.innerText
 
-  //         // TODO: remove all graphs and make it case insensetive
-  //         console.log(text);
-  //         text = text
-  //             .replace(/coronavírus/ig, placeholder)
-  //             .replace(/coronavirus/ig, placeholder)
-  //             .replace(/covid-19/ig, placeholder);
+  //     // TODO: remove all graphs and make it case insensetive
+  //     console.log(text)
+  //     text = text
+  //       .replace(/coronavírus/ig, placeholder)
+  //       .replace(/coronavirus/ig, placeholder)
+  //       .replace(/covid-19/ig, placeholder)
 
-  //         console.log('node', text);
-  //         node.innerText = text;
-  //     }
+  //     console.log('node', text)
+  //     node.innerText = text
+  //   }
   // }
 
-  setNodes() {
-    console.log('hello', this.listOfTextElm)
+  updateNodeList() {
+    console.log('updateNodeList', this.nodes)
+
+    const nodes = document.querySelectorAll(this.searchableNodes.join(','))
+    console.log('updateNodeList', nodes)
+    // console.log('lang', langMessages)
+    // const coronaNodes = this.filterCoronaNodes(nodes)
+    // console.log('coronaNodes', coronaNodes)
+  }
+
+  onDomChanges(mutationsList) {
+    for (const mutation of mutationsList) {
+      if (mutation.addedNodes) {
+        this.updateNodeList()
+      }
+    }
   }
 
   init() {
-    this.setNodes()
+    const { langMessages } = this
+    console.log('langMessages', langMessages)
+
+    if (langMessages) {
+      // @see https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+      const observer = new MutationObserver((mutationList) => this.onDomChanges(mutationList))
+      observer.observe(document.body, { childList: true, subtree: true })
+    }
   }
 }
 
-new TextReplacer().init()
+new AvoidingCorona().init()
